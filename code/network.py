d67b372ac6da867d517b8353e391819df61e0d96
trucleduc <tdlxqb@mail.missouri.edu>
2018-06-24 15:50:09 -0500

    Update network.py

--- code/network.py
+++ code/network.py
@@ -26,6 +26,23 @@ def leak_relu(x, leak=0.1, scope=None):
 
 
 
+def integer_label_to_one_hot_label(integer_label):
+    if (len(integer_label.shape) == 0):
+        one_hot_label = np.zeros((NUM_CATEGORY))
+        one_hot_label[integer_label] = 1
+    elif (len(integer_label.shape) == 4):
+        one_hot_label = np.zeros((N, N, N, K, NUM_SEG_PART))
+        for i in range(N):
+          for j in range(N):
+            for k in range(N):
+              for l in range(K):
+                one_hot_label[i, j, k, l, integer_label[i, j, k, l]] = 1
+    else:
+        one_hot_label = np.ones((1))
+    return one_hot_label
+
+
+
 def pc2voxel(pc, pc_label):
     # Args:
     #     pc: size n x F where n is the number of points and F is feature size
@@ -154,10 +171,11 @@ def get_model(pointgrid, is_training):
     #     pointgrid: of size B x N x N x N x NUM_FEATURES
     #     is_training: boolean tensor
     # Returns:
-    #     pred_label: of size B x N x N x N x (K+1) x NUM_PART_SEG
+    #     pred_cat: of size B x NUM_CATEGORY
+    #     pred_seg: of size B x N x N x N x (K+1) x NUM_PART_SEG
 
+    # Encoder
     batch_size = pointgrid.get_shape()[0].value
-
     conv1 = tf_util.conv3d(pointgrid, 64, [5,5,5], scope='conv1', activation_fn=leak_relu, bn=True, is_training=is_training) # N
     conv2 = tf_util.conv3d(conv1, 64, [5,5,5], scope='conv2', activation_fn=leak_relu, stride=[2,2,2], bn=True, is_training=is_training) # N/2
     conv3 = tf_util.conv3d(conv2, 64, [5,5,5], scope='conv3', activation_fn=leak_relu, bn=True, is_training=is_training) # N/2
@@ -168,7 +186,18 @@ def get_model(pointgrid, is_training):
     conv8 = tf_util.conv3d(conv7, 512, [3,3,3], scope='conv8', activation_fn=leak_relu, stride=[2,2,2], bn=True, is_training=is_training) # N/16
     conv9 = tf_util.conv3d(conv8, 512, [1,1,1], scope='conv9', activation_fn=leak_relu, bn=True, is_training=is_training) # N/16
 
-    deconv1 = tf_util.conv3d_transpose(conv9, 256, [3,3,3], scope='deconv1', activation_fn=leak_relu, bn=True, is_training=is_training, stride=[2,2,2], padding='SAME') # N/8
+    # Classification Network
+    conv9_flat = tf.reshape(conv9, [batch_size, -1])
+    fc1 = tf_util.fully_connected(conv9_flat, 512, activation_fn=leak_relu, bn=True, is_training=is_training, scope='fc1')
+    do1 = tf_util.dropout(fc1, keep_prob=0.7, is_training=is_training, scope='do1')
+    fc2 = tf_util.fully_connected(do1, 256, activation_fn=leak_relu, bn=True, is_training=is_training, scope='fc2')
+    do2 = tf_util.dropout(fc2, keep_prob=0.7, is_training=is_training, scope='do2')
+    pred_cat = tf_util.fully_connected(do2, NUM_CATEGORY, activation_fn=None, bn=False, scope='pred_cat')
+
+    # Segmentation Network
+    cat_features = tf.tile(tf.reshape(tf.concat([fc2, pred_cat], axis=1), [batch_size, 1, 1, 1, -1]), [1, N/16, N/16, N/16, 1])
+    conv9_cat = tf.concat([conv9, cat_features], axis=4)
+    deconv1 = tf_util.conv3d_transpose(conv9_cat, 256, [3,3,3], scope='deconv1', activation_fn=leak_relu, bn=True, is_training=is_training, stride=[2,2,2], padding='SAME') # N/8
     conv7_deconv1 = tf.concat(axis=4, values=[conv7, deconv1])
     deconv2 = tf_util.conv3d(conv7_deconv1, 256, [3,3,3], scope='deconv2', activation_fn=leak_relu, bn=True, is_training=is_training) # N/8
     deconv3 = tf_util.conv3d_transpose(deconv2, 128, [3,3,3], scope='deconv3', activation_fn=leak_relu, bn=True, is_training=is_training, stride=[2,2,2], padding='SAME') # N/4
@@ -181,21 +210,26 @@ def get_model(pointgrid, is_training):
     conv1_deconv7 = tf.concat(axis=4, values=[conv1, deconv7])
     deconv8 = tf_util.conv3d(conv1_deconv7, 64, [5,5,5], scope='deconv8', activation_fn=leak_relu, bn=True, is_training=is_training) # N
 
-    pred_label = tf_util.conv3d(deconv8, (K+1) * NUM_SEG_PART, [5,5,5], scope='pred_label', activation_fn=None, bn=False, is_training=is_training)
-    pred_label = tf.reshape(pred_label, [batch_size, N, N, N, K+1, NUM_SEG_PART])
+    pred_seg = tf_util.conv3d(deconv8, (K+1) * NUM_SEG_PART, [5,5,5], scope='pred_seg', activation_fn=None, bn=False, is_training=is_training)
+    pred_seg = tf.reshape(pred_seg, [batch_size, N, N, N, K+1, NUM_SEG_PART])
 
-    return pred_label
+    return pred_cat, pred_seg
 
 
 
-def get_loss(pred_label, one_hot_label, class_weight):
-    pred_label = tf.reshape(pred_label, [-1, NUM_SEG_PART])
-    one_hot_label = tf.reshape(one_hot_label, [-1, NUM_SEG_PART])
-    #class_weight = tf.reduce_sum(class_weight, axis=0)
-    #weights = tf.reduce_sum(class_weight * one_hot_label, axis=1)
-    per_instance_seg_loss = tf.nn.softmax_cross_entropy_with_logits(logits=pred_label, labels=one_hot_label)
+def get_loss(pred_cat, one_hot_cat, pred_seg, one_hot_seg):
+    per_instance_cat_loss = tf.nn.softmax_cross_entropy_with_logits(logits=pred_cat, labels=one_hot_cat)
+    cat_loss = tf.constant(1000.0, dtype=tf.float32) * tf.reduce_mean(per_instance_cat_loss)
+    per_instance_seg_loss = tf.nn.softmax_cross_entropy_with_logits(logits=pred_seg, labels=one_hot_seg)
     seg_loss = tf.constant(1000.0, dtype=tf.float32) * tf.reduce_mean(per_instance_seg_loss)
-    return seg_loss
+    total_var = tf.trainable_variables()
+    reg_vars = [var for var in total_var if 'weights' in var.name]
+    reg_loss = tf.zeros([], dtype=tf.float32)
+    for var in reg_vars:
+        reg_loss += tf.nn.l2_loss(var)
+    reg_loss = tf.constant(0.01, dtype=tf.float32) * reg_loss
+    total_loss = cat_loss + seg_loss + reg_loss
+    return total_loss, cat_loss, seg_loss
 
 
 
